b44e27cbb653f640183fdce05bffdbb3e8481f33
P_S <priyanka.saxena@ET-C02YQEZ9LVCF.local>
2020-01-28 14:01:56 +0530

    Commit 2.2: Added redo for Player 2. To do: Add leader board.

--- game.java
+++ game.java
@@ -1,7 +1,6 @@
 package ttt;
 
-import java.util.Scanner; 
-
+import java.util.*;
 
 interface board
 {
@@ -11,6 +10,24 @@ interface board
 	public void view();
 } 
 
+class board_state
+{
+    int m;
+    Stack <int[]> state;
+    board_state(int m)
+    {
+        this.m = m;
+        state = new Stack<int[]>();
+    }
+    public void push_s(int [] pos)
+    {
+        state.push(pos);
+    }
+    public int[] pop_s()
+    {
+        return state.pop();
+    }
+}
 
 class ttt_board implements board
 {
@@ -35,7 +52,7 @@ class ttt_board implements board
 			System.out.println("Invalid index. Retry.");
 			return false;
 		}
-		else if(t_board[x-1][y-1] != '_')	
+		else if(t_board[x-1][y-1] != '_' && symbol != '_')	
 		{
 			System.out.println("Cell not empty. Retry");
 			return false;
@@ -354,6 +371,8 @@ public class game
 {
 	public static void main(String[] args) 
 	{
+		board_state b_state = new board_state(4);
+		int[] h_state = new int[4];
 		board9 tb = new board9(3);
 		player p1 = new player();
 		player p2 = new player();
@@ -379,14 +398,40 @@ public class game
 		            p1.input();
 		        }
 		        tb.insert(p1.get_b1(),p1.get_b2(),p1.get_x(),p1.get_y(),'X');
+		        h_state[0]=p1.get_b1();
+		        h_state[1]=p1.get_b2();
+		        h_state[2]=p1.get_x();
+		        h_state[3]=p1.get_y();
+		        b_state.push_s(h_state);
+		        tb.view();
 		    }
 		    else
 		    {
+		        while(true)
+		        {
 		        System.out.println("Player 2's chance.");
 		        p2.input();
 		        tb.insert(p2.get_b1(),p2.get_b2(),p2.get_x(),p2.get_y(),'O');
+		        h_state[0]=p2.get_b1();
+		        h_state[1]=p2.get_b2();
+		        h_state[2]=p2.get_x();
+		        h_state[3]=p2.get_y();
+		        b_state.push_s(h_state);
+		        tb.view();
+		        System.out.println("Enter Y to undo your last move and N otherwise.");
+		        char undo = in.next().charAt(0);
+		        if(undo == 'Y')
+		        {
+		            int[] h_state1 = b_state.pop_s();
+		            tb.insert(h_state1[0], h_state1[1], h_state1[2], h_state1[3], '_');
+		            tb.view();
+		        }
+		        else
+		        {
+		            break;
+		        }
+		        }
 		    }
-		    tb.view();
 		    if(tb.win())
 		    {
 		        if(chance)
